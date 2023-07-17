8057ca12757fdf455d5f838947f2dc1cf19a8f9a
Alexis Guzman <48135533+codingcodax@users.noreply.github.com>
2023-07-17 00:51:23 +0000

    feat(components): create initial form structure

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,33 @@
+import { type SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  one: "string";
+  two: "string";
+  three: "string";
+  four: "string";
+};
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <p>Form</p>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("one", { required: true })} />
+      <input {...register("two", { required: true })} />
+      <input {...register("three", { required: true })} />
+      <input {...register("four", { required: true })} />
+      {errors && <span>Enter a valid code</span>}
+      <button>Verify Email</button>
+    </form>
   );
 };
 
