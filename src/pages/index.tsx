2adfcd59852b78772f6de00e11a7320babe6a8c4
Alexis Guzman <48135533+codingcodax@users.noreply.github.com>
2023-07-17 00:51:44 +0000

    feat(home): add title, description and form component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
+import { Form } from "~/components";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>OTP Password</title>
-        <meta
-          title="description"
-          content="OTP password with react hook form"
-        />
+        <meta title="description" content="OTP password with react hook form" />
       </Head>
-      <div>
-        <p>Home</p>
+      <div className="relative flex min-h-screen flex-col items-center justify-center text-center">
+        <h1 className="text-3xl font-bold">Enter Code</h1>
+        <p className="text-zinc-500">We sent OTP code to your email address</p>
+
+        <Form />
       </div>
     </>
   );
